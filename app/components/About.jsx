738b78dd2b2e8815032c7d0907672eb5164af13a
Baljit Singh <baljitsinghx01@gmail.com>
2016-10-27 22:44:54 +0530

    styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,8 +12,17 @@ var React = require('react');
 var About = (props) => {
 	return (
 			<div>
-				<h3>About component!</h3>
-				<p>Welcome to the about page!</p>
+				<h1>About</h1>
+				<p>This is the weather predictor app made using React, a javascript library. This is my first React app.</p>
+				<p>Here are the tools i used:</p>
+				<ul>
+					<li>
+						<a href="https://facebook.github.io/react">React</a> - This was the javascript framework used.
+					</li>
+					<li>
+						<a href="http://openweathermap.org">Open Weather Map</a> - I used this site to fetch weather data for my app.
+					</li>
+				</ul>
 			</div>
 		)
 };
